f2dad905b2a2b74df664fff544aa8446a15c1c82
Smriti <77229239+Smriti925@users.noreply.github.com>
2022-02-26 23:07:06 +0530

    Update 2011-final-value-of-variable-after-performing-operations.cpp

--- 2011-final-value-of-variable-after-performing-operations/2011-final-value-of-variable-after-performing-operations.cpp
+++ 2011-final-value-of-variable-after-performing-operations/2011-final-value-of-variable-after-performing-operations.cpp
@@ -12,3 +12,16 @@ public:
         }return ans;
     }
 };
+//Solution 2
+// class Solution {
+// public:
+//     int finalValueAfterOperations(vector<string>& operations) {
+//         int ans=0;
+//         for(auto it:operations)
+//         {   if(it=="--X"|| it=="X--")
+//                 ans--;
+//             else
+//                 ans++;
+//         }return ans;
+//     }
+// };
