8933d0c02b9c53933fe8342838d6fd8709ea3088
Smriti <77229239+Smriti925@users.noreply.github.com>
2022-06-25 22:08:32 +0530

    Time: 3 ms (99.88%), Space: 11 MB (30.52%) - LeetHub

--- 1-two-sum/1-two-sum.cpp
+++ 1-two-sum/1-two-sum.cpp
@@ -1,17 +1,23 @@
 class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
-        vector <int> v;
-        for(int i=0;i<nums.size();i++)
-        {
-            for(int j=i+1;j<nums.size();j++)
-            {
-                if(nums[i]+nums[j]==target)
-                {
-                    v.push_back(i);
-                    v.push_back(j);
-                }
+        vector<int> ans;
+        unordered_map<int,int> mpp;
+        for(int i=0;i<nums.size();i++){
+            int a = target-nums[i]; // a+b = target
+        // check whether target-nums[i] is present in hashmap ?
+            
+            
+            if(mpp.find(a)!=mpp.end()){   
+            // if the a is found in the hashmap then return the index of target-b and the current index of a
+                ans.push_back(mpp[a]);
+                ans.push_back(i);
+               break;
             }
-        }return v;
+              // if a is not found int hashmap add it 
+              // add key as nums[i] and value as i
+            mpp[nums[i]] = i;
+        }
+        return ans;
     }
 };
