8f337c077587c9da382849047c6b216fa9b8929b
Smriti <77229239+Smriti925@users.noreply.github.com>
2022-07-25 15:37:47 +0530

    Time: 27 ms (32.45%), Space: 13.3 MB (18.74%) - LeetHub

--- 73-set-matrix-zeroes/73-set-matrix-zeroes.cpp
+++ 73-set-matrix-zeroes/73-set-matrix-zeroes.cpp
@@ -1,35 +1,25 @@
 class Solution {
 public:
     void setZeroes(vector<vector<int>>& matrix) {
-        // vector<int> v;
         int m=matrix.size();
         int n=matrix[0].size();
+        vector<int> v1(m,-1);
+        vector<int> v2(n,-1);
+        
         for(int i=0;i<m;i++){
             for(int j=0;j<n;j++){
                 if(matrix[i][j]==0){
-                    for(int k=0;k<n;k++){
-                        if(matrix[i][k]==0)
-                            matrix[i][k]=0;
-                        else
-                            matrix[i][k]='a';
-                    }
-                    
-                    for(int l=0;l<m;l++){
-                        if(matrix[l][j]==0)
-                            matrix[l][j]=0;
-                        else
-                         matrix[l][j]='a';
-                    }
-                       
+                    v1[i]=0;
+                    v2[j]=0;
                 }
             }
         }
         for(int i=0;i<m;i++){
             for(int j=0;j<n;j++){
-                if(matrix[i][j]=='a'){
+                if(v1[i]==0 || v2[j]==0)
                     matrix[i][j]=0;
-                }
             }
         }
+        return;
     }
 };
