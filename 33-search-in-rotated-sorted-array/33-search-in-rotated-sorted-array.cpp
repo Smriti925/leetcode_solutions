5c8057a1d54cc65c74711344365bc302f0ce23cd
Smriti <77229239+Smriti925@users.noreply.github.com>
2022-08-18 21:15:46 +0530

    Time: 11 ms (21.37%), Space: 11.9 MB (29.48%) - LeetHub

--- 33-search-in-rotated-sorted-array/33-search-in-rotated-sorted-array.cpp
+++ 33-search-in-rotated-sorted-array/33-search-in-rotated-sorted-array.cpp
@@ -7,17 +7,18 @@ public:
             m[nums[i]]=i;
         }
         
-        for(int i=0 ; i<nums.size() ; i++)
-        {
-            if(m.find(target) != m.end())
-            {
-                ans = m[target];
-            }
-        }
-        
-        // for(auto it=m.find(target);it!=m.end();it++){
-        //         ans=it->second;   
+        // for(int i=0 ; i<nums.size() ; i++)
+        // {
+        //     if(m.find(target) != m.end())
+        //     {
+        //         ans = m[target];
         //     }
+        // }
+        
+         for(auto it=m.find(target);it!=m.end();it++){
+                ans=it->second; 
+        break;
+            }
     return ans;
     }
 };
