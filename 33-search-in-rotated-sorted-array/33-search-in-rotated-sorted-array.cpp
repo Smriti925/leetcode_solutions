e519b8192f2b8ca377a677f622d50aec19fb4a2a
Smriti <77229239+Smriti925@users.noreply.github.com>
2022-08-18 21:13:36 +0530

    Time: 12 ms (14.41%), Space: 11.9 MB (29.48%) - LeetHub

---
+++ 33-search-in-rotated-sorted-array/33-search-in-rotated-sorted-array.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    int search(vector<int>& nums, int target) {
+        int ans=-1;
+        unordered_map<int,int> m;
+        for(int i=0;i<nums.size();i++){
+            m[nums[i]]=i;
+        }
+        
+        for(int i=0 ; i<nums.size() ; i++)
+        {
+            if(m.find(target) != m.end())
+            {
+                ans = m[target];
+            }
+        }
+        
+        // for(auto it=m.find(target);it!=m.end();it++){
+        //         ans=it->second;   
+        //     }
+    return ans;
+    }
+};
