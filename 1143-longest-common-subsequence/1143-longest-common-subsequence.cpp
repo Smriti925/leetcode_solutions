e585bc841bbde94fad92d6f8e67922c34ab86611
Smriti <77229239+Smriti925@users.noreply.github.com>
2022-10-15 13:45:43 +0530

    Time: 896 ms (5.18%), Space: 12.9 MB (59.04%) - LeetHub

--- 1143-longest-common-subsequence/1143-longest-common-subsequence.cpp
+++ 1143-longest-common-subsequence/1143-longest-common-subsequence.cpp
@@ -1,22 +1,46 @@
  class Solution {
         public:
-            int longestCommonSubsequence(string s1, string s2) {
-                int n = s1.size();
-                int m = s2.size();
-                vector<int> dp(m+1,0),curr(m+1,0);
-                
-                for(int i =0;i<=n;i++){
-                    for(int j=0;j<=m;j++){
-                        if(i == 0) dp[j] = 0;
-                        else if(j == 0) curr[j] = 0;
-                        else{
-                            if(s1[i-1] == s2[j-1]) curr[j] = 1 + dp[j-1];
-                            else curr[j] = max(dp[j],curr[j-1]);
-                        }
-                    }
-                    dp = curr;
-                }
-                
-                return dp[m];
+            int longestCommonSubsequence(string text1, string text2)
+{
+    int ans = 0;
+
+    int l1 = text1.size();
+    int l2 = text2.size();
+
+    vector<vector<int>> mat(l1 + 1, vector<int>(l2 + 1, 0));
+
+    for (int i = 0; i <= l1; i++)
+    {
+        mat[i][0] = 0;
+    }
+    for (int j = 0; j <= l2; j++)
+    {
+        mat[0][j] = 0;
+    }
+
+    for (int i = 1; i <= l1; i++)
+    {
+        for (int j = 1; j <= l2; j++)
+        {
+            if (text1[i - 1] == text2[j - 1])
+            { // matched
+                mat[i][j] = mat[i - 1][j - 1] + 1;
+            }
+            else
+            {
+                mat[i][j] = max(mat[i - 1][j], mat[i][j - 1]);
+            }
+
+            ans = max(mat[i][j], ans);
+            
+            //matrix check
+            ans=mat[i][j];
+            cout<<ans;
+            }
+            cout<<endl;
+    }
+
+    return ans;
+
             }
         };
